881e0fd42d57e24ae86eb9db86d6e27ed8b529ef
Arnaud Ligny <arnaud@ligny.org>
2015-04-01 02:16:51 +0200

    2015 updates

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,7 @@
 ;(function ($, window, undefined) {
   'use strict';
-
   var $doc = $(document),
       Modernizr = window.Modernizr;
-
   $(document).ready(function() {
     $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
     $.fn.foundationButtons          ? $doc.foundationButtons() : null;
@@ -16,16 +14,8 @@
     $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
     $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
     $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
   });
-
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
   if (Modernizr.touch && !window.location.hash) {
     $(window).load(function () {
@@ -34,7 +24,6 @@
       }, 0);
     });
   }
-
 })(jQuery, this);
 
 // Github/Narno
@@ -44,12 +33,18 @@
       $("div#github-repos dl dt").remove();
       $("div#github-repos dl dd").remove();
       $.each(response.data, function(idx, repo) {
-        if(!repo.fork) {
+        //if(!repo.fork) {
           $("div#github-repos dl").append(
             $("<dt>").append(
               $("<a>")
                 .attr("href", repo["html_url"])
-                .attr("class", "link-github")
+                .attr("class", function(i, val) {
+                  if(repo.fork) {
+                    return "link-fork";
+                  } else {
+                    return "link-github";
+                  }
+                })
                 .append(
                   $("<span>")
                   .html(repo.name)
@@ -62,7 +57,7 @@
                 .html(repo.description)
             )
           );
-        }
+        //}
       });
     });
   });
@@ -74,21 +69,29 @@
       $("div#github-org-repos dl dt").remove();
       $("div#github-org-repos dl dd").remove();
       $.each(response.data, function(idx, repo) {
-        //if(!repo.fork) {
-          $("div#github-org-repos dl").append(
-            $("<dt>").append(
-              $("<a>")
+        $("div#github-org-repos dl").append(
+          $("<dt>").append(
+            $("<a>")
+              .attr("href", repo["html_url"])
+              .attr("class", function(i, val) {
+                  if(repo.fork) {
+                    return "link-fork";
+                  } else {
+                    return "link-github";
+                  }
+                })
+              .append(
+                $("<span>")
                 .html(repo.name)
-                .attr("href", repo["html_url"])
-            )
-          );
-          $("div#github-org-repos dl").append(
-            $("<dd>").append(
-              $("<span>")
-                .html(repo.description)
-            )
-          );
-        //}
+              )
+          )
+        );
+        $("div#github-org-repos dl").append(
+          $("<dd>").append(
+            $("<span>")
+              .html(repo.description)
+          )
+        );
       });
     });
   });
