51065b4e543fb2c8430031639106dbe046cad8b0
Narno <arnaud@ligny.org>
2016-02-07 23:12:22 +0100

    Moving config/options in a YAML file.

--- build.php
+++ build.php
@@ -1,34 +1,38 @@
+#!/usr/local/bin/php
 <?php
 if (php_sapi_name() !== 'cli') {
     return;
 }
+date_default_timezone_set('Europe/Paris');
 require_once 'vendor/autoload.php';
 use PHPoole\PHPoole;
+use Symfony\Component\Yaml\Yaml;
 
-$getopt = getopt('e::');
+$getopt = getopt('e::p::');
 
+$options = Yaml::parse(file_get_contents('.phpoole'));
 $options_dev = [
     'site' => [
-        'title'   => "narno.org",
-        'baseurl' => 'http://localhost:8000/',
-    ],
-    'frontmatter' => [
-        'format' => 'ini'
+        'baseurl' => 'http://localhost:8000',
     ],
 ];
-$options_prod = array_replace_recursive($options_dev, [
-    'site' => [
-        'baseurl' => 'http://narno.org',
-    ],
-]);
 
 $prod = (isset($getopt['e']) && $getopt['e'] == 'prod') ? true : false;
-
-$options = ($prod) ? $options_prod : $options_dev;
+$options = (!$prod) ? array_replace_recursive($options, $options_dev) : $options;
 
 $phpoole = new PHPoole('./', null, $options);
 $phpoole->build();
 
+// run server
 if (!$prod) {
+    echo "Start server http://localhost:8000\n";
+    echo "Ctrl-C to stop it\n";
     exec('php -S localhost:8000 -t _site');
+
+} else {
+// publish?
+    if (isset($getopt['p'])) {
+        echo "Publishing...\n";
+        // @todo implement git push to the gh-pages branch
+    }
 }
