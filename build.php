3b46cd183ed0ef32ef14617e62fe45cd62354634
Narno <arnaud@ligny.org>
2016-02-08 02:03:14 +0100

    Try to publish...

--- build.php
+++ build.php
@@ -7,6 +7,8 @@ date_default_timezone_set('Europe/Paris');
 require_once 'vendor/autoload.php';
 use PHPoole\PHPoole;
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 $getopt = getopt('e::p::');
 
@@ -34,5 +36,32 @@ if (!$prod) {
     if (isset($getopt['p'])) {
         echo "Publishing...\n";
         // @todo implement git push to the gh-pages branch
+        function run($command) {
+            exec($command.' 2>&1', $output, $returnValue);
+            $output = implode(PHP_EOL, $output);
+            return "$output\n";
+        }
+        $filesystem = new Filesystem();
+        $branch = 'master';
+        $tmpDirectory = tempnam(sys_get_temp_dir(), 'phpoole_publish_');
+        //
+        $filesystem->remove($tmpDirectory);
+        $filesystem->mkdir($tmpDirectory);
+        //
+        echo run("git clone git@github.com:Narno/narno.github.com.git $tmpDirectory");
+        //
+        echo run("cd \"$tmpDirectory\" && git checkout $branch");
+        //
+        $finder = new Finder();
+        $finder->files()
+            ->in($tmpDirectory)
+            ->ignoreVCS(true);
+        $filesystem->remove($finder);
+        $filesystem->mirror('_site', $tmpDirectory);
+        echo run("cd \"$tmpDirectory\" && git add --all . && git commit -m \"Website generated with PHPoole\"");
+        //
+        echo run("cd \"$tmpDirectory\" && git push origin $branch");
+        //
+        $filesystem->remove($tmpDirectory);
     }
 }
